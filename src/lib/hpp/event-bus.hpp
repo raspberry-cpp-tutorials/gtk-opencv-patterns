45e93059de067a31b1adb8c469c02b1e044ccf76
jean-michel-gonet <jmgonet@yahoo.com>
2018-12-19 07:47:07 +0100

    Add the event bus, as decoupling strategy

---
+++ src/lib/hpp/event-bus.hpp
@@ -0,0 +1,87 @@
+#ifndef EVENT_BUS_HPP
+#define EVENT_BUS_HPP
+
+#include <vector>
+
+using namespace std;
+
+/**
+ * A class to receive events and redistribute them to
+ * all subscribers.
+ * This piece of code involves all kind of difficult stuff I had to learn:
+ * - Using static variables in template classes:
+ *   - https://www.fayewilliams.com/2014/12/23/static-variables-in-template-classes-and-methods/
+ * - Why do we need to have a header-only class:
+ *   - https://stackoverflow.com/questions/1639797/template-issue-causes-linker-error-c
+ * - Use erase-remove idiom to remove elements by value from a vector:
+     - https://stackoverflow.com/questions/3385229/c-erase-vector-element-by-value-rather-than-by-position
+ * - How to deal with static members in header-only classes:
+ *   - https://stackoverflow.com/questions/11709859/how-to-have-static-data-members-in-a-header-only-library
+ */
+
+/** @brief Describes the interface that a subscriber must implement.
+ * The subscriber has to implement the ability to receive events.
+ */
+template <class T>
+class Subscriptor {
+public:
+	/** @brief Calls the subscriptor providing the event.
+	 * @param event The event.
+	 */
+	virtual void receive(T event) = 0;
+};
+
+/** @brief A static vector of Subscriptor.
+ * This is a work around to use a static variables in header-only
+ * classes.
+ */
+template <class T>
+struct SubscriptorsHolder {
+	static vector<Subscriptor<T> *> subscriptors;
+};
+
+/** @brief Declare the static variable, so it can be linked.
+ */
+template <class T>
+vector<Subscriptor<T> *> SubscriptorsHolder<T>::subscriptors;
+
+/** @brief Propagates the events to all subscriptors in the list.
+ */
+template <class T>
+class EventBusService {
+public:
+	virtual ~EventBusService() = default;
+	
+	/** @brief empties the list of subscribers.
+	 * You should not use this outside unit testing.
+	 */
+	virtual void clear() {
+		SubscriptorsHolder<T>::subscriptors.clear();
+	}
+	 
+	/** @brief Propagates the event to all subscriptors.
+	 * @param event The event.
+	 */
+	virtual void propagate(T event) {
+		for (unsigned int i = 0; i < SubscriptorsHolder<T>::subscriptors.size(); i++) {
+			Subscriptor<T> *subscriptor = SubscriptorsHolder<T>::subscriptors[i];
+			subscriptor->receive(event);
+		}
+	};
+
+	/** @brief Any class able to receive the event can subscribe.
+	 * @param subscriptor A pointer to an instance able to receive events.
+	 */
+	virtual void subscribe(Subscriptor<T> *subscriptor) {
+		SubscriptorsHolder<T>::subscriptors.push_back(subscriptor);
+	};
+
+	/** @brief Unsubscribes the specified subscriptor.
+	 * @param subscriptor A pointer to the subscriptor.
+	 */
+	virtual void unsubscribe(Subscriptor<T> *subscriptor) {
+		SubscriptorsHolder<T>::subscriptors.erase(remove(SubscriptorsHolder<T>::subscriptors.begin(), SubscriptorsHolder<T>::subscriptors.end(), subscriptor), SubscriptorsHolder<T>::subscriptors.end());
+	}
+};
+
+#endif
---
+++ src/lib/tst/event-bus-test.cpp
@@ -0,0 +1,124 @@
+#include "event-bus.hpp"
+#include "catch.hpp"
+
+/** Any class can be used as an event. */
+class AnyEvent {
+public:
+	AnyEvent(int id) {
+		identity = id;
+	}
+	int getIdentity() {
+		return identity;
+	}
+protected:
+	int identity;
+};
+
+/** Let's have another event. */
+class OtherEvent:public AnyEvent {
+public:
+	OtherEvent(int id):AnyEvent(id) {
+		// Nothing to do.
+	}
+};
+
+/** Any class can be a subscriber, as long as it
+  * implements the ability to receive events. */
+class SubscriberToAllEvents:public Subscriptor<AnyEvent>, public Subscriptor<OtherEvent> {
+public:
+	SubscriberToAllEvents(int id) {
+		identityOfLastReceivedEvent = id;
+	};
+	void receive(AnyEvent event) override {
+		identityOfLastReceivedEvent = event.getIdentity();
+	}
+	void receive(OtherEvent event) override {
+		identityOfLastReceivedEvent = event.getIdentity();
+	}
+	int getIdentityOfLastReceivedEvent() {
+		return identityOfLastReceivedEvent;
+	}
+	void setIdentityOfLastReceivedEvent(int id) {
+		identityOfLastReceivedEvent = id;
+	}
+private:
+	int identityOfLastReceivedEvent;
+};
+
+using namespace std::placeholders;
+using namespace std;
+
+SCENARIO("The Event Bus Service broadcasts to all subscribers of this event") {
+	EventBusService<AnyEvent> eventBus;
+
+	GIVEN( "Two classes subscribed to the same event") {
+		SubscriberToAllEvents subscriber1(10), subscriber2(20);
+		eventBus.clear();
+		eventBus.subscribe(&subscriber1);
+		eventBus.subscribe(&subscriber2);
+
+		WHEN( "When the event is fired") {
+			eventBus.propagate(AnyEvent(1));
+			THEN ( "All subscriptors are informed ") {
+				REQUIRE( subscriber1.getIdentityOfLastReceivedEvent() == 1);
+				REQUIRE( subscriber2.getIdentityOfLastReceivedEvent() == 1);
+			}
+		}
+		
+		WHEN( "One class unsubscribes") {
+			eventBus.unsubscribe(&subscriber2);
+			
+			THEN ("It doesn't receive events any more") {
+				eventBus.propagate(AnyEvent(2));
+				REQUIRE( subscriber1.getIdentityOfLastReceivedEvent() == 2);
+				REQUIRE( subscriber2.getIdentityOfLastReceivedEvent() == 20);
+			}
+		}
+	}
+}
+
+
+SCENARIO("The Event Bus Service broatcasts only to subscribers of this event") {
+	EventBusService<AnyEvent> eventBus1;
+	EventBusService<OtherEvent> eventBus2;
+	
+	GIVEN( "Two classes subscribed to different event") {
+		eventBus1.clear();
+		eventBus2.clear();
+		SubscriberToAllEvents subscriber1(10), subscriber2(20);
+		eventBus1.subscribe(&subscriber1);
+		eventBus2.subscribe(&subscriber2);
+		
+		WHEN( "When the event is fired") {
+			eventBus1.propagate(AnyEvent(1));
+			eventBus2.propagate(OtherEvent(2));
+
+			THEN ( "Only subscribers to specific event are informed ") {
+				REQUIRE( subscriber1.getIdentityOfLastReceivedEvent() == 1);
+				REQUIRE( subscriber2.getIdentityOfLastReceivedEvent() == 2);
+			}
+		}
+	}
+}
+
+SCENARIO("The Event Bus Service is a singleton") {
+	EventBusService<AnyEvent> eventBus1;
+	EventBusService<AnyEvent> eventBus2;
+	
+	GIVEN( "Two classes subscribed to same event using different instances of same event bus") {
+		eventBus1.clear();
+		eventBus2.clear();
+		SubscriberToAllEvents subscriber1(10), subscriber2(20);
+		eventBus1.subscribe(&subscriber1);
+		eventBus2.subscribe(&subscriber2);
+		
+		WHEN( "When the event is fired") {
+			eventBus1.propagate(AnyEvent(1));
+			
+			THEN ( "All subscriptors are informed") {
+				REQUIRE( subscriber1.getIdentityOfLastReceivedEvent() == 1);
+				REQUIRE( subscriber2.getIdentityOfLastReceivedEvent() == 1);
+			}
+		}
+	}
+}
