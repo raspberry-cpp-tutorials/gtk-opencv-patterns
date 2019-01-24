c3621030b07d721837137137e52d91ec79d7e4ac
jean-michel-gonet <jmgonet@yahoo.com>
2019-01-24 17:46:28 +0100

    Removed unused buttons and labels

--- src/app/cpp/main-window.cpp
+++ src/app/cpp/main-window.cpp
@@ -6,41 +6,18 @@
 
 MainWindow::MainWindow(int width, int height):
 probablyInFullScreen(false),
-m_button("Hello World"),
-m_box(Gtk::ORIENTATION_VERTICAL),
-m_label1("First Label"),
 movieMaker(ServiceLocator::getMovieMaker()){
     // Configure this window:
     this->set_default_size(width, height);
 
-    // Connect the 'click' signal and make the button visible:
-    m_button.signal_clicked().connect(
-            sigc::mem_fun(*this, &MainWindow::buttonClick));
-    m_button.show();
-
-    // Make the first label visible:
-    m_label1.show();
-
     // Make the second label visible:
     cameraDrawingArea.show();
 
-    // Pack all elements in the box:
-    m_box.pack_start(m_button, Gtk::PACK_SHRINK);
-    m_box.pack_start(m_label1, Gtk::PACK_SHRINK);
-    m_box.pack_start(cameraDrawingArea, Gtk::PACK_EXPAND_WIDGET);
-
     // Add the box in this window:
-    add(m_box);
-
-    // Make the box visible:
-    m_box.show();
+    add(cameraDrawingArea);
 
     // Activate Key-Press events
     add_events(Gdk::KEY_PRESS_MASK);
-}
-
-void MainWindow::buttonClick() {
-    std::cout << "Hello World" << std::endl;
 }
 
 bool MainWindow::on_key_press_event(GdkEventKey* event) {
--- src/app/hpp/main-window.hpp
+++ src/app/hpp/main-window.hpp
@@ -14,11 +14,7 @@ protected:
     bool on_key_press_event(GdkEventKey* event) override;
 
 private:
-    void buttonClick();
     bool probablyInFullScreen;
-    Gtk::Button m_button;
-    Gtk::Box m_box;
-    Gtk::Label m_label1;
     CameraDrawingArea cameraDrawingArea;
     MovieMaker * movieMaker;
 };
